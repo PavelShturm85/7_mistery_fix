f4eeab1629009df221aa4678db8bec1ebfcc3170
PavelShturm85 <pavel.shturm@gmail.com>
2017-11-17 03:18:22 +0400

    внесены изменения по чек листу

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,20 +2,10 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    # a=0 makes the function linear, not quadratic.
-    if a == 0:
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
         return None, None
     else:
-        discriminant = b ** 2 - 4 * a * c
-        # The square root is extracted only from a nonnegative number.
-        if discriminant < 0:
-            return None, None
-        # If discriminant is 0, then roots of the quadratic equation 1.
-        elif discriminant == 0:
-            root1 = (-b - sqrt(discriminant)) / (2 * a)
-            return root1, None
-        # If discriminant > 0, then roots of the quadratic equation 2.
-        else:
-            root1 = (-b - sqrt(discriminant)) / (2 * a)
-            root2 = (-b + sqrt(discriminant)) / (2 * a)
-            return root1, root2
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        return root1, root2 if discriminant > 0 else None
